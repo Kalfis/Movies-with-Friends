ae92da4df3c11addc3f237e0e908613155a5e7e0
Maggie Love <maggie.love1991@gmail.com>
2015-11-21 17:15:08 -0500

    created user model with bcrypt hashing

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 'use strict';
 let mongoose = require('mongoose');
 let bcrypt = require('bcrypt');
+// Ddefine the rounds/iterations the bcrypy key setup phase uses
+SALT_WORK_FACTOR = 10;
 
 let userSchema = new mongoose.Schema({
   username: String,
@@ -12,6 +14,37 @@ let userSchema = new mongoose.Schema({
   to_watch_list: []
   // consider adding comments to user?
 });
+
+// Before saving a password, make sure it is encrypted.
+userSchema.pre('save', (next) => {
+    let user = this;
+
+  // hash the password only if it's new or has been modified
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
+
+    // hash the password and the newly generated salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      // override cleartext password with hashed password
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Implement password verification
+User.methods.authenticate = (password, callback) => {
+  // compare method that returns a boolean
+  // Determine if the first argument once encrypted corres. to the second argument
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    callback( null, isMatch);
+  });
+}
 
 let user = mongoose.model('User', userSchema);
 
