005baa0035b5621f1a0591eb5e09aed754f20089
LUIS ARCE <luisarce5@gmail.com>
2015-11-21 20:29:02 -0500

    Added in movies_controller route /searchByTitle/:title, to search by title and render movie info for that title

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -5,26 +5,33 @@ let User = require('../models/user');
 
 let Movie = require('../models/movie.js')  // requires access to the model in movie.js
 
-router.route('/') // creates / route within /movies, i.e. /movies/
-      .get((req, res, next) => {
-        console.log ('hit / route in /movies');
-        Movie.find([], (err, movie) => {
-          if(err) throw err;
-          console.log('this is the movie data');
-          res.send(movie);
-        });
-      })
-        .post((req, res, next) => {
-          // let newUser = new User({
-          //   name: req.body.name,
-          //   password: req.body.password
-          // });
-          // console.log(newUser);
-          // newUser.save((err) => {
-          //   if (err) throw err;
-          //   console.log('new user saved');
-          // });
-          // res.send('saved');
-        });
+// create '/' route within /movies.  Accessed in browser at /movies/
+// this route displays all of the database's contents, i.e. the profiles of each of
+// the movies that have been selected by Users.
+router.route('/')
+.get((req, res, next) => {
+  console.log ('hit / route in /movies => /movies/');
+  Movie.find([], (err, movie) => {
+    if(err) throw err;
+    res.send(movie);
+    console.log('this is all the contents in the movies datatabase');
+  });
+});
 
-  module.exports = router;
+// search by title for movie in mymovies collection in database
+router.route('/searchByTitle/:title')
+  .get((req, res, next) => {
+    console.log ('hit /movies/searchByTitle:/:title');
+    var title = req.params.title;
+    Movie.findOne({ title: title}, (err, movie) => {
+      if(err) return next(err);
+      if (movie == null) {
+        res.send("The Movie you searched for is not in 'mymovies'.");
+      };
+      console.log('movie profile searched by title accessed.');
+      console.log("This is the data: " + movie);
+      res.send(movie);
+    });
+  });
+
+module.exports = router;
