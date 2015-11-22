a0a3325ed261545f3d162ce9433918e7a8974aa0
LUIS ARCE <luisarce5@gmail.com>
2015-11-21 21:52:21 -0500

    In movies controller, added router for /searchByTitle/:title.  Under this, included routes to search for movie by title to: 1) display movie info (GET), 2) Edit movie info (PUT), and 3) Delete movie info (DELETE)

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -8,6 +8,7 @@ let Movie = require('../models/movie.js')  // requires access to the model in mo
 // create '/' route within /movies.  Accessed in browser at /movies/
 // this route displays all of the database's contents, i.e. the profiles of each of
 // the movies that have been selected by Users.
+// tested OK in browser
 router.route('/')
 .get((req, res, next) => {
   console.log ('hit / route in /movies => /movies/');
@@ -18,8 +19,10 @@ router.route('/')
   });
 });
 
-// search by title for movie in mymovies collection in database
+// Sets router constructor
 router.route('/searchByTitle/:title')
+// SEARCHES by title for movie in mymovies collection in database
+// tested OK in browser
   .get((req, res, next) => {
     console.log ('hit /movies/searchByTitle:/:title');
     var title = req.params.title;
@@ -31,6 +34,30 @@ router.route('/searchByTitle/:title')
       console.log('movie profile searched by title accessed.');
       console.log("This is the data: " + movie);
       res.send(movie);
+    });
+  })
+  
+  // EDITS  a movie profile, selected by its title from database
+  // pending to test in browser as it requires AJAX code in
+  // public/js/script to be completed
+  .put((req, res) => {
+    var title = req.params.title;
+    Movie.findOneAndUpdate({ _title: title}, { $set: req.body }, (err, movie) => {
+      if(err) console.log(err);
+      res.send(movie);
+      console.log("Movie updated");
+    });
+  })
+
+  // DELETES a movie profile, selected by its title from database
+  // pending to test in browser as it requires AJAX code in
+  // public/js/script to be completed
+  .delete((req, res) => {
+    var title = req.params.title;
+    Movie.findOneAndRemove({ _title: title}, (err, movie) => {
+      if(err) console.log(err);
+      console.log("Movie deleted from mymovies collection");
+      res.send("Movie deleleted from my movies")
     });
   });
 
