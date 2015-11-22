81803f158960d7dccd53e09cd64871f5ad6a1c03
Maggie Love <maggie.love1991@gmail.com>
2015-11-22 00:22:06 -0500

    created seed.js file--still working on defining individual users/saving them

--- seed.js
+++ seed.js
@@ -1,1 +1,31 @@
 'use strict';
+
+let User = require('./models/user')
+let Movie = require('./models/movie')
+let mongoose = require('mongoose');
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/moviegoerApp');
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', (callback) => {
+  console.log('Mongoose Connected');
+});
+
+// should username be plural?
+let username = [ 'MGustave', 'Zero', 'Agatha' ]
+let bio = [
+  'I appreciate the finer things in life. Always looking for the next great adventure.',
+  'Along for the ride--especially if the ride involves cakes.'
+  'Firm believer in happy endings.'
+]
+let password = [
+  'password1',
+  'password2',
+  'password3'
+]
+// leaving watched_list empty for now, to test API data retrieval later.
+let watched_list = [];
+// leaving watched_list empty for now, to test API data retrieval later.
+let to_watch_list = [];
