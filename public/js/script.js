84e54f2be60eec8f7356355852ef908e3b141825
LUIS ARCE <luisarce5@gmail.com>
2015-11-22 23:33:29 -0500

    added newMovies function in script.js to show profiles of released movies thru DOM in index.html; added related event listener for API-Releases-button with corresponding Ajax function

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 $(function(){
+
+  // var renderTemplate_movies = Handlebars.compile($('template#movies').html));
+
   console.log ('index.html linked to script.js');
 
   //===== Event listener for API button to retrieve & display information for a hardcoded movie from API
@@ -17,10 +20,33 @@ $(function(){
         url: 'https://api.themoviedb.org/3/movie/550?api_key=5c47d1a627613469f840623448f6e67b'
       }).done(function(data){
         console.log('movie title selected');
-        // $('#results-container').empty();
+        $('#movie-profile').empty();
         showMovie(data);
       });
     }); // close #submit-button
+
+    //===== Event listener for API button to retrieve & display  releaased movies from API
+    //======================================
+
+    $('#API-Releases-button').click(function(event){
+      event.preventDefault();
+
+      console.log('Clicked Submit Button');
+
+      var titleInput = $('#title-input').val();
+      console.log(titleInput);
+
+      $.ajax({
+        url: 'https://api.themoviedb.org/3/discover/movie?api_key=5c47d1a627613469f840623448f6e67b&primary_release_date.gte=2015-10-15&primary_release_date.lte=2015-11-22'
+      }).done(function(movieObjs){
+        console.log('movies released selected');
+        $('#movie-profile').empty();
+        newMovies(movieObjs);
+      });
+    }); // close #API-releases-button
+
+    // url: 'https://api.themoviedb.org/3/discover/movie?api_key=5c47d1a627613469f840623448f6e67b&primary_release_date.gte=2015-10-15&primary_release_date.lte=2015-11-22'
+url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=5c47d1a627613469f840623448f6e67b'
 
     //===== Display all information from database in console log while on Index Page
     //======================================
@@ -85,12 +111,72 @@ $(function(){
   //   })
   // })
 
-  var editMovie = function () {
+  // var editMovie = function () { // PENDING
+  //   $.ajax({
+  //     url: 'http://localhost:3000/movies/searchByTitle/'+titleInput.
+  //     method: "GET",
+  //     dataType: "json"
+  //   }).done(updateForm)
+  // };
 
-  }
+
+  // Render information of a Released Movies thru DOM in index.html
+  //======================================
+
+  var newMovies = function(movieObjs){
+
+        console.log(movieObjs);
+        // for (var i = 0; i < movieObjs.length; i++){
+        //   var showMovies = movieObjs[i].data;
+
+            $.each( movieObjs, function(key, value){
+              console.log("now key + value");
+              console.log( key + " : " + value);
+            });
+
+            var result = $('#movie-profile').append('<div>').find('div');
+            result.attr('class', 'movie');
+
+            console.log("length " + movieObjs.results.length);
+
+            for (var i=0; i < movieObjs.results.length; i++){
+            // $.each( movieObjs, function(key, value){
+              // console.log(key,value);
+              console.log (movieObjs.results[i].title);
+
+                result.append('<p><strong> Title: </strong>'+ movieObjs.results[i].title + '</p>');
+                result.append('<img src=https://image.tmdb.org/t/p/w185' + movieObjs.results[i].poster_path + '></img>');
+                result.append('<p><strong>  Released Date: </strong>'+ movieObjs.results[i].release_date + '</p>');
+              // });
+            };
+    };
 
 
-  // render information of a movie profile thru DOM in index.html
+
+
+
+///////
+// var showData = function(data){
+//       // using JavaScript to render info on the DOM
+//       console.log(data);
+//
+//       $.each( data, function(key, value){
+//         console.log( key + " : " + value);
+//       }); // checking in console how data displays before sending to the DOM
+//
+//       var result = $('#results-container').append('<div>').find('div');
+//       result.attr('class', 'movie');
+//
+//       $.each( data, function(key, value){
+//         result.append('<p>' + key + " : " + value + '</p>');
+//       });
+//
+//     }; // close showData
+
+
+
+
+  // Render information of a movie profile thru DOM in index.html
   //======================================
   var showMovie = function(data){
     // using JavaScript to render info on the DOM
@@ -115,5 +201,8 @@ $(function(){
     // });
 
   }; // close showMovie
+
+
+
 
 }) // close main anonymous function
