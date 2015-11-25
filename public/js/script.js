3276a3e868c62d3f30131db993401931b96f238b
LUIS ARCE <luisarce5@gmail.com>
2015-11-24 19:32:33 -0500

    testing formats for reference to heroku server instead of local host

--- public/js/script.js
+++ public/js/script.js
@@ -112,7 +112,8 @@ $(function(){
           $.ajax({
               // url: 'http://localhost:3000/movies/searchByTitle/'+titleInput
               // url: 'http://localhost:3000/movies/searchByTitle/'+ titleInput || process.env.PORT + '/movies/searchByTitle/' + titleInput
-              url: process.env.PORT + '/movies/searchByTitle/' + titleInput
+              // url: process.env.PORT + '/movies/searchByTitle/' + titleInput
+              url: '/movies/searchByTitle/' + titleInput
             }).done(function(data){
               console.log('movie title selected');
               $('#movie-profile').empty();
